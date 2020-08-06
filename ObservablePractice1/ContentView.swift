f680e947944826975d9af52f9920039b0651b37b
Kelly Douglass <kdouglass@gmail.com>
2020-08-06 14:01:51 -0500

    Initial Commit

---
+++ ObservablePractice1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ObservablePractice1
+//
+//  Created by Kelly Douglass on 8/6/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
